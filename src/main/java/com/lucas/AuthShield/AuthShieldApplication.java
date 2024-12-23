666a152306253db83537747de6af48dc67cd1782
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-12-23 15:04:49 -0300

    first commit

---
+++ src/main/java/com/lucas/AuthShield/AuthShieldApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.AuthShield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/AuthShield/AuthShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.AuthShield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
